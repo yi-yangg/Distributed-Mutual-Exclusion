3013120affbe236516cda5eb3e465e6c3b299804
ycho0097 <52307200+Octagoat@users.noreply.github.com>
2023-11-03 21:43:21 +1100

    finished proposed algorithm

--- lodha_kshemkalyani.c
+++ lodha_kshemkalyani.c
@@ -2,6 +2,7 @@
 #include <mpi.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #define N 4
 #define REQUEST 0
@@ -15,8 +16,8 @@ typedef struct Request {
 } Request;
 
 // Initialize local state for process
-int size, rank;
-int My_Sequence_Number = 0
+int size, rank, iter_count;
+int My_Sequence_Number = 0;
 bool RV[N];
 bool deferredProcs[N];
 Request* LRQ = NULL;
@@ -30,22 +31,26 @@ MPI_Datatype requestType;
 int request_flag = NONE;
 MPI_Request request_req;
 // Function prototypes
-
+void requestCs();
 void InvMutEx();
-void RcvReq();
-void RcvReply();
-void FinCs();
-void RcvFlsuh();
+void RcvReq(Request req, bool isRequesting);
+void RcvReplyOrFlush(Request req);
+void FinCS();
 bool CheckExecuteCs();
 void resetRV();
 void freeLRQ();
 void resetDP();
+int getLRQCount(Request req);
+void removeReqFromLRQ(Request req);
+void insertIntoLRQ(Request req);
+void listenIncomingRequest(bool isRequesting);
 
 int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    resetRv();
+    int csInterest = rank % 2;
+    resetRV();
 
     MPI_Datatype type[2] = {MPI_INT, MPI_INT};
     int blocklen[2] = {1, 1};
@@ -63,6 +68,36 @@ int main(int argc, char* argv[]) {
     request.sequence_number = 0;
     request.process_id = rank;
 
+    for (int i = 0; i < 1; i++) {
+        if (csInterest) {
+
+            // request critical section from all other processes
+            requestCs();
+            printf("-----------------------------------------------------------------\n");
+            printf("Process %d is in the critical section\n", rank);
+            // simulate work inside critical section
+            printf("Currently in critical section\n");
+            sleep(1);
+
+            printf("Process %d exiting critical section\n", rank);
+            printf("-----------------------------------------------------------------\n");
+            fflush(stdout);
+            FinCS();
+        }
+        else {
+            iter_count = 0;
+            int max_iter = N / 2; // max number of odd rank processes
+            while (iter_count < max_iter) {
+                listenIncomingRequest(false);
+            }
+        }
+
+        
+    }
+    MPI_Finalize();
+
+    return 0;
+
 
 }
 
@@ -76,29 +111,37 @@ void requestCs() {
     Request response_req[N-1];
     // Recv response from other processes
     for (int j = 0; j < N; j++) {
-        if (j != rank)
-            MPI_Irecv(&response_req[req_count], 1, requestType, j, MPI_ANY_TAG, MPI_COMM_WORLD, &req[req_count++]);
+        if (j != rank) {
+            MPI_Irecv(&response_req[req_count], 1, requestType, j, MPI_ANY_TAG, MPI_COMM_WORLD, &req[req_count]);
+            req_count++;
+        }
+            
     }
 
 
     for (int i = 0; i < req_count; i++) {
         while (true) {
             MPI_Test(&req[i], &flags[i], &stat[i]);
-            listenIncomingRequest();
+            listenIncomingRequest(true);
             if (flags[i]) {
                 break;
             }
         }
 
         if (stat[i].MPI_TAG == REQUEST) {
-            RcvReq(req[i], true);
+            RcvReq(response_req[i], true);
         }
         else {
-            RcvReplyOrFlush(req[i]);
+            RcvReplyOrFlush(response_req[i]);
         }
-
+        int recv_rank = response_req[i].process_id;
         if (CheckExecuteCs()) {
             break;
+        }
+        else if (LRQ[0].process_id != rank) {
+            MPI_Recv(&response_req[i], 1, requestType, recv_rank, FLUSH, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("Received flush from %d\n", recv_rank);
+            RcvReplyOrFlush(response_req[i]);
         }
     }
     
@@ -137,7 +180,7 @@ void RcvReq(Request req, bool isRequesting) {
 
     if(isRequesting) {
         if (!RV[sender]) {
-            insertReqIntoLRQ(req);
+            insertIntoLRQ(req);
             RV[sender] = true;
         }
         else {
@@ -145,23 +188,31 @@ void RcvReq(Request req, bool isRequesting) {
         }
     }
     else {
-        MPI_Send(request, 1, requestType, sender, REPLY, MPI_COMM_WORLD);
+        MPI_Send(&request, 1, requestType, sender, REPLY, MPI_COMM_WORLD);
     }
 }
 
 void RcvReplyOrFlush(Request req) {
-    int SN = req.sequence_number;
     int sender = req.process_id;
 
-    RV[sender] = 1;
+    RV[sender] = true;
     removeReqFromLRQ(req);
 
 }
 
 void FinCS() {
     removeReqFromLRQ(request);
-    Request nextReq = LRQ[0];
-    MPI_Send(request, 1, requestType, nextReq.process_id, FLUSH, MPI_COMM_WORLD);
+    if (LRQ_size > 0) {
+        Request nextReq = LRQ[0];
+        printf("Sending flush to %d\n", nextReq.process_id);
+        MPI_Send(&request, 1, requestType, nextReq.process_id, FLUSH, MPI_COMM_WORLD);
+    }
+    
+    for (int i = 0; i < N; i++) {
+        if (deferredProcs[i]) {
+            MPI_Send(&request, 1, requestType, i, REPLY, MPI_COMM_WORLD);
+        }
+    }
 }
 bool CheckExecuteCs() {
     for (int i = 0; i < N; i++) {
@@ -169,8 +220,7 @@ bool CheckExecuteCs() {
             return false;
         }
     }
-
-    return LRQ.process_id[0] == rank;
+    return LRQ[0].process_id == rank;
 }
 
 void resetRV() {
@@ -188,7 +238,7 @@ void freeLRQ() {
 
 void resetDP() {
     for (int j = 0; j < N; j++) {
-        deferrredProcs[j] = false;
+        deferredProcs[j] = false;
     }
 }
 
@@ -234,20 +284,16 @@ void insertIntoLRQ(Request req) {
 void listenIncomingRequest(bool isRequesting) {
     
     if (request_flag == NONE) {
-        MPI_Irecv(received_request, 1, requestType, MPI_ANY_SOURCE, REQUEST, MPI_COMM_WORLD, &request_req);
+        MPI_Irecv(&received_request, 1, requestType, MPI_ANY_SOURCE, REQUEST, MPI_COMM_WORLD, &request_req);
     }
 
     MPI_Test(&request_req, &request_flag, MPI_STATUS_IGNORE);
 
     // if the process currently requesting
-    // check priority between Pi and Pj
     if (request_flag) {
-        int Pj_SN = received_request.sequence_number;
-        int Pj_rank = received_request.process_id;
-        
         RcvReq(received_request, isRequesting);
 
         request_flag = NONE; // reset request_flag to setup another async listener
-
+        iter_count++;
     }
 }
